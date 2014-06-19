253bd6acd263877f3d8e43eaeec7723cdbc249ee
David Baena <baena5999@gmail.com>
2014-06-19 21:27:50 +0200

    Fixed set passwd

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
 default['one']['path']="/var/lib/one"
 default['one']['sunstone']['port']="8080"
-#default[:one][:sunstone][:binder]="0.0.0.0"
+default['one']['sunstone']['binder']="0.0.0.0"
+  
+default['one']['oneadmin']['user']="oneadmin"
+default['one']['oneadmin']['password']="oneadmin"
--- recipes/admin.rb
+++ recipes/admin.rb
@@ -16,14 +16,6 @@ package "opennebula-sunstone"
 log "Installing nfs-server..."
 package "nfs-kernel-server"
 
-log "Setting credentials"
-template "#{node['one']['path']}/.one/one_auth" do
- source "one_auth.erb"
- owner "oneadmin"
- group "root"
- mode 0600
-end
-
 log "Setting sunstone-config"
 template "/etc/one/sunstone-server.conf" do
  source "sunstone-server.conf.erb"
@@ -34,21 +26,23 @@ end
 
 service "opennebula-sunstone" do
   action :restart
-
 end
 
+log "Setting credentials"
 execute "change passwd" do
   command "oneuser passwd 0 oneadmin"
+  environment ({'ONE_AUTH' => "#{node['one']['path']}/.one/one_auth"})
   user "oneadmin"
+end
+
+template "#{node['one']['path']}/.one/one_auth" do
+ source "one_auth.erb"
+ owner "oneadmin"
+ group "root"
+ mode 0600
 end
 
 service "opennebula" do
   action :restart
 end
-#
-#include_recipe "database::mysql"
-#mysql_connection_info = {
-#  :host     => 'localhost',
-#  :username => 'root',
-#  :password => node['mysql']['server_root_password']
-#}
+
